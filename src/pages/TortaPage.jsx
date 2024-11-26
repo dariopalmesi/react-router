67f19ac976a1ff2c3af9e0dc179318485f67345b
dariopalmesi <dariopalmesi88@gmail.com>
2024-11-26 17:31:44 +0100

    upgrade commit

--- src/pages/TortaPage.jsx
+++ src/pages/TortaPage.jsx
@@ -14,6 +14,7 @@ export default function TortaPage() {
                 .then(res => res.json())
                 .then(data => {
                     console.log(data);
+                    setTorta(data.data)
 
 
                 })
@@ -30,7 +31,15 @@ export default function TortaPage() {
             {
                 torta ? (
                     <div>
-                        I not found
+                        <div className="card p-3 m-3">
+                            <h3 className='mb-3'>{torta.title}</h3>
+                            <p>{torta.slug}</p>
+                            <p>{torta.content}</p>
+                            <img className="tortaImage" src={`http://localhost:3000/img/${torta.image}`} alt="" />
+                            <p>{torta.tags}</p>
+
+                        </div>
+
                     </div>
                 ) : (
                     <div>
