e2b3ea816a8d5f0b894f228d1b444ebca9055c26
dariopalmesi <dariopalmesi88@gmail.com>
2024-11-26 17:18:48 +0100

    upgrade torta page commit

--- src/pages/TortaPage.jsx
+++ src/pages/TortaPage.jsx
@@ -1,14 +1,46 @@
 import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
 
 
 export default function TortaPage() {
+
+    const [torta, setTorta] = useState(null)
     const { slug } = useParams()
     const url = `http://localhost:3000/posts/${slug}`
     console.log(url);
+    useEffect(
+        () => {
+            fetch(url)
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
 
+
+                })
+                .catch(err => {
+                    console.log(err);
+
+                })
+        }, [])
 
 
     return (
-        <h1>Pizza :slug {slug}</h1>
+        <>
+            <h1>Torta :slug {slug}</h1>
+            {
+                torta ? (
+                    <div>
+                        I not found
+                    </div>
+                ) : (
+                    <div>
+                        loading...
+                    </div>
+                )
+            }
+
+        </>
+
+
     )
 }
